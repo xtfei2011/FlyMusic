b9d5ecdb848f9178b69dc18e35f1637d7c9f72d4
谢腾飞 <xtfei_2011@126.com>
2017-08-14 08:52:55 +0800

    Initial Commit

---
+++ FlyMusic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlyMusic
+//
+//  Created by 谢腾飞 on 2017/8/14.
+//  Copyright © 2017年 谢腾飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlyMusic/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FlyMusic
+//
+//  Created by 谢腾飞 on 2017/8/14.
+//  Copyright © 2017年 谢腾飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
